507e47ceeb3f254f8f82e37c48eff59049d63675
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 23:34:01 -0500

    class Correo: atributo, constructores, getters y setters

---
+++ src/main/java/org/Jhonatan/Agenda/Modelo/Correo.java
@@ -0,0 +1,21 @@
+package org.Jhonatan.Agenda.Modelo;
+
+public class Correo {
+    private String usuario;
+
+    public Correo(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Correo() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+}
