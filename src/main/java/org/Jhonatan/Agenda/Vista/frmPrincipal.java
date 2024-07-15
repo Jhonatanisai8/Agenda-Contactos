687629570b0e4fe444a2410f9ec3d3bb4621abb2
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 23:20:58 -0500

    class frmPrincipal: Botones del menu

--- src/main/java/org/Jhonatan/Agenda/Vista/frmPrincipal.java
+++ src/main/java/org/Jhonatan/Agenda/Vista/frmPrincipal.java
@@ -54,6 +54,12 @@ public class frmPrincipal extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         lblNombre = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
+        btn_prin = new javax.swing.JButton();
+        btn_lends = new javax.swing.JButton();
+        btn_returns = new javax.swing.JButton();
+        btn_users = new javax.swing.JButton();
+        btn_books = new javax.swing.JButton();
+        btn_reports = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         lblAdmin = new javax.swing.JLabel();
         lblFecha = new javax.swing.JLabel();
@@ -73,6 +79,102 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         jSeparator1.setBackground(new java.awt.Color(255, 255, 255));
 
+        btn_prin.setBackground(new java.awt.Color(153, 153, 153));
+        btn_prin.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_prin.setForeground(new java.awt.Color(255, 255, 255));
+        btn_prin.setText("Principal");
+        btn_prin.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_prin.setBorderPainted(false);
+        btn_prin.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_prin.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_prin.setIconTextGap(13);
+        btn_prin.setInheritsPopupMenu(true);
+        btn_prin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_prinActionPerformed(evt);
+            }
+        });
+
+        btn_lends.setBackground(new java.awt.Color(153, 153, 153));
+        btn_lends.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_lends.setForeground(new java.awt.Color(255, 255, 255));
+        btn_lends.setText("Préstamos");
+        btn_lends.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_lends.setBorderPainted(false);
+        btn_lends.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_lends.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_lends.setIconTextGap(13);
+        btn_lends.setInheritsPopupMenu(true);
+        btn_lends.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_lendsActionPerformed(evt);
+            }
+        });
+
+        btn_returns.setBackground(new java.awt.Color(153, 153, 153));
+        btn_returns.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_returns.setForeground(new java.awt.Color(255, 255, 255));
+        btn_returns.setText("Devoluciones");
+        btn_returns.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_returns.setBorderPainted(false);
+        btn_returns.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_returns.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_returns.setIconTextGap(13);
+        btn_returns.setInheritsPopupMenu(true);
+        btn_returns.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_returnsActionPerformed(evt);
+            }
+        });
+
+        btn_users.setBackground(new java.awt.Color(153, 153, 153));
+        btn_users.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_users.setForeground(new java.awt.Color(255, 255, 255));
+        btn_users.setText("Usuarios");
+        btn_users.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_users.setBorderPainted(false);
+        btn_users.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_users.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_users.setIconTextGap(13);
+        btn_users.setInheritsPopupMenu(true);
+        btn_users.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_usersActionPerformed(evt);
+            }
+        });
+
+        btn_books.setBackground(new java.awt.Color(153, 153, 153));
+        btn_books.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_books.setForeground(new java.awt.Color(255, 255, 255));
+        btn_books.setText("Libros");
+        btn_books.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_books.setBorderPainted(false);
+        btn_books.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_books.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_books.setIconTextGap(13);
+        btn_books.setInheritsPopupMenu(true);
+        btn_books.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_booksActionPerformed(evt);
+            }
+        });
+
+        btn_reports.setBackground(new java.awt.Color(153, 153, 153));
+        btn_reports.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btn_reports.setForeground(new java.awt.Color(255, 255, 255));
+        btn_reports.setText("Reportes");
+        btn_reports.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 13, 1, 1, new java.awt.Color(0, 0, 0)));
+        btn_reports.setBorderPainted(false);
+        btn_reports.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_reports.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btn_reports.setIconTextGap(13);
+        btn_reports.setInheritsPopupMenu(true);
+        btn_reports.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_reportsActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -85,7 +187,18 @@ public class frmPrincipal extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
                         .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addContainerGap(29, Short.MAX_VALUE))
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(btn_lends, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_users, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_prin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_returns, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
+                        .addComponent(btn_books, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_reports, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addContainerGap()))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,6 +208,27 @@ public class frmPrincipal extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createSequentialGroup()
+                    .addGap(176, 176, 176)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(50, 50, 50)
+                            .addComponent(btn_lends, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(150, 150, 150)
+                            .addComponent(btn_users, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btn_prin, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(100, 100, 100)
+                            .addComponent(btn_returns, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(200, 200, 200)
+                            .addComponent(btn_books, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGap(250, 250, 250)
+                            .addComponent(btn_reports, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addContainerGap(176, Short.MAX_VALUE)))
         );
 
         jPanel3.setBackground(new java.awt.Color(204, 204, 204));
@@ -106,7 +240,7 @@ public class frmPrincipal extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblFecha, javax.swing.GroupLayout.DEFAULT_SIZE, 570, Short.MAX_VALUE)
+                    .addComponent(lblFecha, javax.swing.GroupLayout.DEFAULT_SIZE, 558, Short.MAX_VALUE)
                     .addComponent(lblAdmin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(168, 168, 168))
         );
@@ -167,6 +301,30 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_prinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_prinActionPerformed
+        //  ShowJPanel(new Principal());
+    }//GEN-LAST:event_btn_prinActionPerformed
+
+    private void btn_lendsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_lendsActionPerformed
+        // ShowJPanel(new Lendings());
+    }//GEN-LAST:event_btn_lendsActionPerformed
+
+    private void btn_returnsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_returnsActionPerformed
+        // ShowJPanel(new Returns());
+    }//GEN-LAST:event_btn_returnsActionPerformed
+
+    private void btn_usersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_usersActionPerformed
+        // ShowJPanel(new Users());
+    }//GEN-LAST:event_btn_usersActionPerformed
+
+    private void btn_booksActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_booksActionPerformed
+        //  ShowJPanel(new Books());
+    }//GEN-LAST:event_btn_booksActionPerformed
+
+    private void btn_reportsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_reportsActionPerformed
+        // ShowJPanel(new Reports());
+    }//GEN-LAST:event_btn_reportsActionPerformed
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -176,6 +334,12 @@ public class frmPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_books;
+    private javax.swing.JButton btn_lends;
+    private javax.swing.JButton btn_prin;
+    private javax.swing.JButton btn_reports;
+    private javax.swing.JButton btn_returns;
+    private javax.swing.JButton btn_users;
     private static javax.swing.JPanel contenido;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
