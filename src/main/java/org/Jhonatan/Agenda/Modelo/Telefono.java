5464f1485ee08e8458d70a1f1c07cabd7814eeb3
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-14 23:32:39 -0500

    class Telefono: atributo, constructores, getters y setters

---
+++ src/main/java/org/Jhonatan/Agenda/Modelo/Telefono.java
@@ -0,0 +1,21 @@
+package org.Jhonatan.Agenda.Modelo;
+
+public class Telefono {
+    private int numero;
+
+    public Telefono() {
+    }
+
+    public Telefono(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
